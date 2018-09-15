3e180c243f8f3b709a0c5f3400e14f9a98a2a657
Cragsdale1996 <cragsdale@smu.edu>
2018-09-14 22:47:07 -0500

    Created hangman directory in lib, added Hangman.Game module

---
+++ hangman/lib/hangman/game.ex
@@ -0,0 +1,18 @@
+defmodule Hangman.Game do
+    @moduledoc """
+    Hangman game backend.
+    """
+
+    def new_game() do
+        :new_game
+    end
+
+    def tally(_game) do
+        :tally
+    end
+
+    def make_move(_game, _guess) do
+        :make_move
+    end
+    
+end
