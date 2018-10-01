258460c6e8ba5ee21155c3dbf778b8c8ec5e1710
Cragsdale1996 <cragsdale@smu.edu>
2018-09-30 23:35:26 -0500

    finishing touches

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -20,10 +20,18 @@ defmodule Hangman.Game do
     ### Public Functions (Called from API) ###
 
     @spec new_game(binary()) :: struct()
-    def new_game(word), do: %Game{ word: word |> String.downcase() }
+    def new_game(word) do 
+        %Game{ 
+            word: word |> String.downcase() 
+        }
+    end
 
     @spec new_game() :: struct()
-    def new_game(), do: %Game{ word: Dictionary.random_word() |> String.downcase() }
+    def new_game() do 
+        %Game{ 
+            word: Dictionary.random_word() |> String.downcase() 
+        }
+    end
 
     @spec tally(struct()) :: map()
     def tally(game = %Game{}) do
@@ -43,16 +51,22 @@ defmodule Hangman.Game do
     def make_move(game = %Game{ status: status }, _guess) 
         when status in [:won, :lost]
     do
-        { game, tally(game) }
+        { 
+            game, 
+            game |> tally() 
+        }
     end 
 
     def make_move(game = %Game{}, guess) do
 
-        new_state = game
-                    |> check_for_duplicate_guess(guess)
-                    |> process_guess(guess)
+        new_game_state = game
+                         |> check_for_duplicate_guess(guess)
+                         |> process_and_score_guess(guess)
 
-        { new_state, tally(new_state) }
+        { 
+            new_game_state, 
+            new_game_state |> tally() 
+        }
 
     end
 
@@ -61,7 +75,8 @@ defmodule Hangman.Game do
     defp mask_unguessed_letters(word, _guessed, status) 
         when status in [:won, :lost] 
     do
-        word |> String.codepoints()
+        word 
+        |> String.codepoints()
     end
 
     defp mask_unguessed_letters(word, guessed, _status) do
@@ -90,14 +105,16 @@ defmodule Hangman.Game do
         end
     end
 
-    defp process_guess(game = %Game{ status: :already_used }, _guess), do: game
-    defp process_guess(game = %Game{ status: :initializing }, guess) do
+    defp process_and_score_guess(game = %Game{ status: :already_used }, _guess) do 
+        game
+    end
+
+    defp process_and_score_guess(game = %Game{ status: :initializing }, guess) do
         game
         |> add_guess_to_state(guess)
         |> score_guess(guess)
         |> check_for_win_or_loss()
     end
-
 
     defp add_guess_to_state(game = %Game{}, guess) do
         %{ game |
@@ -120,11 +137,20 @@ defmodule Hangman.Game do
         end
     end
 
-    defp check_for_win_or_loss(game = %Game{ turns_left: 0 }), do: %{ game | status: :lost }
+    defp check_for_win_or_loss(game = %Game{ turns_left: 0 }) do 
+        %{ game | 
+            status: :lost 
+        }
+    end
+
     defp check_for_win_or_loss(game = %Game{ guessed: guessed, word: word, status: status }) do
         cond do
-            won_game?(guessed, word, status) -> %{ game | status: :won }
-            true                             -> game
+            won_game?(guessed, word, status) -> 
+                %{ game | 
+                    status: :won 
+                }
+            true -> 
+                game
         end
     end
 
